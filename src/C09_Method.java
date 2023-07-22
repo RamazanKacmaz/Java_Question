857ea02da72454581e325378f5ea4c5b21f6a9b8
RamazanKacmaz <98986481+RamazanKacmaz@users.noreply.github.com>
2023-07-22 21:19:07 +0200

    Initial commit

---
+++ src/C09_Method.java
@@ -0,0 +1,32 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+import java.util.logging.SocketHandler;
+
+public class C09_Method {
+    /*Soru 18-) Method Sorusu
+    Girilen araç hızı ve yol kilometresinden, gidilen yere varış süresini hesaplayan
+     bir method yazalım. Kullanıcıya hızını ve kilometreyi soralım, bunu methoda gönderelim
+      ve bize varış süremizi hesaplasın
+    İpucu:
+    yol=Hız*zaman*/
+
+    public static void main(String[] args) {
+
+        aracinvarissuresi();
+
+    }
+
+    private static void aracinvarissuresi() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen hizinizi giriniz..");
+        double hiz = scanner.nextDouble();
+        System.out.println("Lütfen km giriniz");
+        double km = scanner.nextDouble();
+
+        double sure = km / hiz ;
+
+        System.out.println("Varis suresi : " + sure);
+
+    }
+}
